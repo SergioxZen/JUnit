d27e2358e85b9caa2ffa4d62bb814308876ca92f
unknown <pst@A22-15.itstep.lan>
2017-09-11 19:38:57 +0300

    second commit

---
+++ src/main/java/by/itstep/Calculator.java
@@ -0,0 +1,13 @@
+package by.itstep;
+
+public class Calculator {
+
+    public Number plus(Integer x1, Integer x2){
+        return x1 + x2;
+    }
+
+    
+
+
+
+}
---
+++ src/main/java/by/itstep/Main.java
@@ -0,0 +1,10 @@
+package by.itstep;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
